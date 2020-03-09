69b0abd10ebe0eb213fbf5b3956d1824336faf84
Wander Bravo <wander.bravo@gmail.com>
2020-03-09 16:09:40 -0500

    create User sample model admin

--- creceEcuador/app/models.py
+++ creceEcuador/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    usuario = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        """ string para User"""
+        return self.nombres + " " + self.apellidos
