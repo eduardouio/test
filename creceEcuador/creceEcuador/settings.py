700c32b3f2285c6680b5a6402973eda4eeb00659
Wander Bravo <wander.bravo@gmail.com>
2020-11-08 20:14:12 -0500

    Remover credenciales de email

--- creceEcuador/creceEcuador/settings.py
+++ creceEcuador/creceEcuador/settings.py
@@ -65,8 +65,8 @@ SESSION_COOKIE_AGE = 86400 #24 horas de sesion
 #Email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'info@creceecuador.com'
-EMAIL_HOST_PASSWORD = 'crece01GYE'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 MIDDLEWARE = [
