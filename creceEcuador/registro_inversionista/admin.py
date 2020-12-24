a3df2c57200acc06766ded7e00cac44d98b5179d
Wander Bravo <wander.bravo@gmail.com>
2020-12-24 09:13:07 -0500

    Add export_as_csv option in admin for Usuarios

--- creceEcuador/registro_inversionista/admin.py
+++ creceEcuador/registro_inversionista/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from . import models
+from django.http import HttpResponse
+import csv
 # Register your models here.
-admin.site.register(models.Usuario)
+#admin.site.register(models.Usuario)
 admin.site.register(models.Encuesta)
 admin.site.register(models.Pregunta)
 admin.site.register(models.Respuesta)
@@ -12,3 +14,23 @@ admin.site.register(models.Fuente_ingresos)
 admin.site.register(models.Canton)
 admin.site.register(models.Contrato)
 admin.site.register(models.VersionContrato)
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+    export_as_csv.short_description = "Export Selected"
+
+@admin.register(models.Usuario)
+class UsuarioInversionistaAdmin(admin.ModelAdmin, ExportCsvMixin):
+    actions = ["export_as_csv"]
