53a954e894484e8cce17cd724de1f5c8fe3faaa1
Wander Bravo <wander.bravo@gmail.com>
2021-10-24 12:21:16 -0500

    Remove output

--- creceEcuador/scripts/migrate.sh
+++ creceEcuador/scripts/migrate.sh
@@ -2,6 +2,6 @@
 cd /home/ec2-user/viewflow-crece/creceEcuador/
 source /home/ec2-user/viewflow-crece/env/bin/activate
 
-/home/ec2-user/viewflow-crece/env/bin/python manage.py makemigrations
-/home/ec2-user/viewflow-crece/env/bin/python manage.py migrate
-/home/ec2-user/viewflow-crece/env/bin/python manage.py collectstatic 
+/home/ec2-user/viewflow-crece/env/bin/python manage.py makemigrations > /dev/null
+/home/ec2-user/viewflow-crece/env/bin/python manage.py migrate > /dev/null
+/home/ec2-user/viewflow-crece/env/bin/python manage.py collectstatic --no-input > /dev/null
