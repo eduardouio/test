c0fc21e1ca5baeac79d3162ddcb28f4bc1bfbf21
Wander Bravo <wander.bravo@gmail.com>
2021-10-24 01:43:31 -0500

    Fix stop script issue

--- creceEcuador/scripts/stop.sh
+++ creceEcuador/scripts/stop.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 cd /home/ec2-user/viewflow-crece/creceEcuador/
 source /home/ec2-user/viewflow-crece/env/bin/activate
 # supervisorctl -c /home/ec2-user/viewflow-crece/creceEcuador/supervisord.conf stop all 2&>1 >/dev/null
