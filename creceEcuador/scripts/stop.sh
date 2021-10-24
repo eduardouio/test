64a7fec6f2f4e7269f8ef9e168e3789a43eb9201
Wander Bravo <wander.bravo@gmail.com>
2021-10-24 01:35:27 -0500

    Fix stop app

--- creceEcuador/scripts/stop.sh
+++ creceEcuador/scripts/stop.sh
@@ -2,4 +2,4 @@
 cd /home/ec2-user/viewflow-crece/creceEcuador/
 source /home/ec2-user/viewflow-crece/env/bin/activate
 # supervisorctl -c /home/ec2-user/viewflow-crece/creceEcuador/supervisord.conf stop all 2&>1 >/dev/null
-sudo pkill supervisor*
+sudo pkill supervisord
