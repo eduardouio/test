ba59fd742e157ee8907beecc81c63d547ece082e
Wander Bravo <wander.bravo@gmail.com>
2021-10-24 01:28:28 -0500

    Stop app/start app fix

--- creceEcuador/scripts/migrate.sh
+++ creceEcuador/scripts/migrate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/www/project/
-source /home/ec2-user/www/project-venv/bin/activate
+cd /home/ec2-user/viewflow-crece/creceEcuador/
+source /home/ec2-user/viewflow-crece/env/bin/activate
 
 /home/ec2-user/viewflow-crece/env/bin/python manage.py makemigrations
 /home/ec2-user/viewflow-crece/env/bin/python manage.py migrate
